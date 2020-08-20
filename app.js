31926e5d0fb327967332b8843204d9ae32ebed25
Tyler Degand <tylerdegand@gmail.com>
2020-08-20 09:05:43 -0500

    styles and script updated to give it a more user friendly feel

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const calculateTips = () => {
     const bill = document.getElementById('amount').value;
     const percentage = document.getElementById('percentage').value;
     const people = document.getElementById('people').value;
+    const peopleLabel = document.getElementById('peopleLabel')
 
     let tip = bill * (percentage / 100);
     tip = tip.toFixed(2);
@@ -12,9 +13,16 @@ const calculateTips = () => {
     let share = tip / people;
 
     if (people == '' || people == null) {
+        peopleLabel.textContent = 'How many people?';
+        peopleLabel.style.color = ''
         document.getElementById('share').innerHTML = "Your tip!";
         document.getElementById('finalAmount').innerHTML = "$" + tip;
+    } else if (people < 1) {
+        peopleLabel.textContent = 'Please Enter a valid number of people';
+        peopleLabel.style.color = "#f5ed00";
     } else {
+        peopleLabel.textContent = 'How many people?'
+        peopleLabel.style.color = ''
         document.getElementById('share').innerHTML = "Your Share!";
         document.getElementById('finalAmount').innerHTML = "$" + share;
     }    
